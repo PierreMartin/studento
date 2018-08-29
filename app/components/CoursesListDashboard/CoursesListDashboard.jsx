e8d7c65d7ab6ed51aa2e73e7941520c7f8989506
Pierre Martin <pierre.martin.prestataire@sfr.com>
2018-08-29 16:13:21 +0200

    CoursesListDashboard - convert 'CoursesListDashboard' to Class component

--- app/components/CoursesListDashboard/CoursesListDashboard.jsx
+++ app/components/CoursesListDashboard/CoursesListDashboard.jsx
@@ -1,74 +1,137 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router';
 import moment from 'moment';
-import { Icon, Table, Button, Menu, Header } from 'semantic-ui-react';
+import { Icon, Table, Button, Menu, Header, Modal } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { deleteCourseAction } from '../../actions/courses';
 
-const renderCoursesList = (courses, handleOpenModalForDeleteCourse) => {
-	if (courses.length === 0) return 'No yet courses';
+class CoursesListDashboard extends Component {
+	constructor(props) {
+		super(props);
+		this.handleOpenModalForDeleteCourse = this.handleOpenModalForDeleteCourse.bind(this);
+		this.handleSubmitDeleteCourse = this.handleSubmitDeleteCourse.bind(this);
 
-	return courses.map((course, key) => {
-		const courseDate = moment(course.created_at).format('L');
+		this.state = {
+			deleteCourse: {
+				isModalOpened: false,
+				courseId: '',
+				courseTitle: ''
+			}
+		};
+	}
+
+	handleOpenModalForDeleteCourse = (param) => {
+		return () => {
+			const { courseId, courseTitle } = param;
+
+			// Open modal :
+			if (typeof courseId === 'string' && courseId.length > 0) {
+				return this.setState({ deleteCourse: { isModalOpened: true, courseId, courseTitle } });
+			}
+
+			// Close modal :
+			this.setState({ deleteCourse: { isModalOpened: false, courseId: '', courseTitle: '' } });
+		};
+	};
+
+	handleSubmitDeleteCourse() {
+		const { deleteCourseAction } = this.props;
+		const { deleteCourse } = this.state;
+
+		if (deleteCourse && deleteCourse.courseId) {
+			deleteCourseAction(deleteCourse).then(() => {
+				// Close modal :
+				this.setState({ deleteCourse: { isModalOpened: false, courseId: '', courseTitle: '' } });
+			});
+		}
+	}
+
+	renderCoursesList(courses) {
+		if (courses.length === 0) return 'No yet courses';
+
+		return courses.map((course, key) => {
+			const courseDate = moment(course.created_at).format('L');
+
+			return (
+				<Table.Row key={key}>
+					<Table.Cell>
+						<Header as="h4">
+							<Header.Content><Header.Subheader>{course.uId.username}</Header.Subheader></Header.Content>
+						</Header>
+					</Table.Cell>
+					<Table.Cell><a href={`/course/${course._id}`}>{course.title}</a></Table.Cell>
+					<Table.Cell>
+						<Header as="h4">
+							<Header.Content><Header.Subheader>{courseDate}</Header.Subheader></Header.Content>
+						</Header>
+					</Table.Cell>
+					<Table.Cell><Icon color="red" name="star" /> 121</Table.Cell>
+					<Table.Cell><Button color="grey" content="Edit" icon="settings" size="tiny" as={Link} to={`/course/edit/${course._id}`} /></Table.Cell>
+					<Table.Cell><Button inverted color="red" content="Delete" icon="remove" size="tiny" onClick={this.handleOpenModalForDeleteCourse({ courseId: course._id, courseTitle: course.title })} /></Table.Cell>
+				</Table.Row>
+			);
+		});
+	}
+
+	render() {
+		const { courses } = this.props;
+		const { deleteCourse } = this.state;
 
 		return (
-			<Table.Row key={key}>
-				<Table.Cell>
-					<Header as="h4">
-						<Header.Content><Header.Subheader>{course.uId.username}</Header.Subheader></Header.Content>
-					</Header>
-				</Table.Cell>
-				<Table.Cell><a href={`/course/${course._id}`}>{course.title}</a></Table.Cell>
-				<Table.Cell>
-					<Header as="h4">
-						<Header.Content><Header.Subheader>{courseDate}</Header.Subheader></Header.Content>
-					</Header>
-				</Table.Cell>
-				<Table.Cell><Icon color="red" name="star" /> 121</Table.Cell>
-				<Table.Cell><Button color="grey" content="Edit" icon="settings" size="tiny" as={Link} to={`/course/edit/${course._id}`} /></Table.Cell>
-				<Table.Cell><Button inverted color="red" content="Delete" icon="remove" size="tiny" onClick={handleOpenModalForDeleteCourse({ courseId: course._id, courseTitle: course.title })} /></Table.Cell>
-			</Table.Row>
+			<div>
+				<Table celled unstackable compact="very">
+					<Table.Header>
+						<Table.Row>
+							<Table.HeaderCell>Author</Table.HeaderCell>
+							<Table.HeaderCell>Title</Table.HeaderCell>
+							<Table.HeaderCell>Date</Table.HeaderCell>
+							<Table.HeaderCell>Stars</Table.HeaderCell>
+							<Table.HeaderCell>Edit</Table.HeaderCell>
+							<Table.HeaderCell>Delete</Table.HeaderCell>
+						</Table.Row>
+					</Table.Header>
+
+					<Table.Body>
+						{ this.renderCoursesList(courses) }
+					</Table.Body>
+
+					<Table.Footer fullWidth>
+						<Table.Row>
+							<Table.HeaderCell colSpan="6">
+								<Button basic color="grey" content="Add new course" floated="right" icon="add" as={Link} to="/course/create/new" />
+
+								<Menu pagination>
+									<Menu.Item as="a" icon><Icon name="chevron left" /></Menu.Item>
+									<Menu.Item as="a">1</Menu.Item>
+									<Menu.Item as="a">2</Menu.Item>
+									<Menu.Item as="a" icon><Icon name="chevron right" /></Menu.Item>
+								</Menu>
+							</Table.HeaderCell>
+						</Table.Row>
+					</Table.Footer>
+				</Table>
+
+				<Modal open={deleteCourse.isModalOpened} onClose={this.handleOpenModalForDeleteCourse({})}>
+					<Modal.Header>Delete a course</Modal.Header>
+					<Modal.Content image>
+						<Modal.Description>
+							<Header>Are you sure to delete the course "{deleteCourse.courseTitle}"?</Header>
+							<p>ATTENTION this action is irreversible!</p>
+						</Modal.Description>
+					</Modal.Content>
+					<Modal.Actions>
+						<Button color="black" onClick={this.handleOpenModalForDeleteCourse({})}>Cancel</Button>
+						<Button icon="checkmark" color="red" labelPosition="right" content="Ok" onClick={this.handleSubmitDeleteCourse} />
+					</Modal.Actions>
+				</Modal>
+			</div>
 		);
-	});
-};
-
-const CoursesListDashboard = ({ courses, handleOpenModalForDeleteCourse }) => {
-	return (
-		<Table celled unstackable compact="very">
-			<Table.Header>
-				<Table.Row>
-					<Table.HeaderCell>Author</Table.HeaderCell>
-					<Table.HeaderCell>Title</Table.HeaderCell>
-					<Table.HeaderCell>Date</Table.HeaderCell>
-					<Table.HeaderCell>Stars</Table.HeaderCell>
-					<Table.HeaderCell>Edit</Table.HeaderCell>
-					<Table.HeaderCell>Delete</Table.HeaderCell>
-				</Table.Row>
-			</Table.Header>
-
-			<Table.Body>
-				{ renderCoursesList(courses, handleOpenModalForDeleteCourse) }
-			</Table.Body>
-
-			<Table.Footer fullWidth>
-				<Table.Row>
-					<Table.HeaderCell colSpan="6">
-						<Button basic color="grey" content="Add new course" floated="right" icon="add" as={Link} to="/course/create/new" />
-
-						<Menu pagination>
-							<Menu.Item as="a" icon><Icon name="chevron left" /></Menu.Item>
-							<Menu.Item as="a">1</Menu.Item>
-							<Menu.Item as="a">2</Menu.Item>
-							<Menu.Item as="a" icon><Icon name="chevron right" /></Menu.Item>
-						</Menu>
-					</Table.HeaderCell>
-				</Table.Row>
-			</Table.Footer>
-		</Table>
-	);
-};
+	}
+}
 
 CoursesListDashboard.propTypes = {
-	handleOpenModalForDeleteCourse: PropTypes.func,
+	deleteCourseAction: PropTypes.func,
 
 	courses: PropTypes.arrayOf(PropTypes.shape({
 		_id: PropTypes.string,
@@ -80,4 +143,10 @@ CoursesListDashboard.propTypes = {
 	})).isRequired
 };
 
-export default CoursesListDashboard;
+const mapStateToProps = (state) => {
+	return {
+		courses: state.courses.all
+	};
+};
+
+export default connect(mapStateToProps, { deleteCourseAction })(CoursesListDashboard);
--- app/pages/Dashboard.jsx
+++ app/pages/Dashboard.jsx
@@ -1,30 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { fetchCoursesByIdAction, deleteCourseAction } from '../actions/courses';
+import { fetchCoursesByIdAction } from '../actions/courses';
 import CoursesListDashboard from '../components/CoursesListDashboard/CoursesListDashboard';
 import LayoutPage from '../components/layouts/LayoutPage/LayoutPage';
-import { Segment, Container, Header, Modal, Button } from 'semantic-ui-react';
+import { Segment, Container, Header } from 'semantic-ui-react';
 // import classNames from 'classnames/bind';
 // import styles from '../css/main.scss';
 
 // const cx = classNames.bind(styles);
 
 class Dashboard extends Component {
-	constructor(props) {
-		super(props);
-		this.handleOpenModalForDeleteCourse = this.handleOpenModalForDeleteCourse.bind(this);
-		this.handleSubmitDeleteCourse = this.handleSubmitDeleteCourse.bind(this);
-
-		this.state = {
-			deleteCourse: {
-				isModalOpened: false,
-				courseId: '',
-				courseTitle: ''
-			}
-		};
-	}
-
 	componentDidMount() {
 		const { userMe, fetchCoursesByIdAction } = this.props;
 		fetchCoursesByIdAction(userMe._id);
@@ -38,35 +24,8 @@ class Dashboard extends Component {
 		};
 	}
 
-	handleOpenModalForDeleteCourse = (param) => {
-		return () => {
-			const { courseId, courseTitle } = param;
-
-			// Open modal :
-			if (typeof courseId === 'string' && courseId.length > 0) {
-				return this.setState({ deleteCourse: { isModalOpened: true, courseId, courseTitle } });
-			}
-
-			// Close modal :
-			this.setState({ deleteCourse: { isModalOpened: false, courseId: '', courseTitle: '' } });
-		};
-	};
-
-	handleSubmitDeleteCourse() {
-		const { deleteCourseAction } = this.props;
-		const { deleteCourse } = this.state;
-
-		if (deleteCourse && deleteCourse.courseId) {
-			deleteCourseAction(deleteCourse).then(() => {
-				// Close modal :
-				this.setState({ deleteCourse: { isModalOpened: false, courseId: '', courseTitle: '' } });
-			});
-		}
-	}
-
 	render() {
 		const { courses } = this.props;
-		const { deleteCourse } = this.state;
 
 		return (
 			<LayoutPage {...this.getMetaData()}>
@@ -78,22 +37,8 @@ class Dashboard extends Component {
 
 					<Container text>
 						<Header as="h2" icon="list" content="My courses" style={{ fontSize: '1.7em', fontWeight: 'normal' }} />
-						<CoursesListDashboard courses={courses} handleOpenModalForDeleteCourse={this.handleOpenModalForDeleteCourse} />
+						<CoursesListDashboard courses={courses} />
 					</Container>
-
-					<Modal open={deleteCourse.isModalOpened} onClose={this.handleOpenModalForDeleteCourse({})}>
-						<Modal.Header>Delete a course</Modal.Header>
-						<Modal.Content image>
-							<Modal.Description>
-								<Header>Are you sure to delete the course "{deleteCourse.courseTitle}" ?</Header>
-								<p>ATTENTION this action is irreversible !</p>
-							</Modal.Description>
-						</Modal.Content>
-						<Modal.Actions>
-							<Button color="black" onClick={this.handleOpenModalForDeleteCourse({})}>Cancel</Button>
-							<Button icon="checkmark" color="red" labelPosition="right" content="Ok" onClick={this.handleSubmitDeleteCourse} />
-						</Modal.Actions>
-					</Modal>
 
 				</Segment>
 			</LayoutPage>
@@ -103,7 +48,6 @@ class Dashboard extends Component {
 
 Dashboard.propTypes = {
 	fetchCoursesByIdAction: PropTypes.func,
-	deleteCourseAction: PropTypes.func,
 
 	courses: PropTypes.arrayOf(PropTypes.shape({
 		_id: PropTypes.string,
@@ -126,4 +70,4 @@ const mapStateToProps = (state) => {
 	};
 };
 
-export default connect(mapStateToProps, { fetchCoursesByIdAction, deleteCourseAction })(Dashboard);
+export default connect(mapStateToProps, { fetchCoursesByIdAction })(Dashboard);
