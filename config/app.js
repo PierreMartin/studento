a5ccc98401eb98dc969e431c5838d837ab93aa18
Pierre Martin <pierre.martin.prestataire@sfr.com>
2018-09-25 13:04:30 +0200

    change domaine name

--- config/app.js
+++ config/app.js
@@ -4,7 +4,7 @@ export const isProduction = ENV === 'production';
 export const isDebug = ENV === 'development';
 export const isClient = typeof window !== 'undefined';
 
-export const apiEndpoint = isDebug ? 'http://localhost:3000' : 'https://shielded-headland-62294.herokuapp.com';
+export const apiEndpoint = isDebug ? 'http://localhost:3000' : 'https://studento.herokuapp.com';
 
 // url images:
 export const pathImage = isDebug ? '/uploads' : URL_IMAGES;
