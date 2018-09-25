de59cbbc0c4e501c45b212925fc5ca6d6824beef
Pierre Martin <pierre.martin.prestataire@sfr.com>
2018-09-25 10:44:42 +0200

    Avatar - add var env URL_IMAGES

--- config/app.js
+++ config/app.js
@@ -1,4 +1,4 @@
-import { ENV } from './env';
+import { ENV, URL_IMAGES } from './env';
 
 export const isProduction = ENV === 'production';
 export const isDebug = ENV === 'development';
@@ -7,7 +7,7 @@ export const isClient = typeof window !== 'undefined';
 export const apiEndpoint = isDebug ? 'http://localhost:3000' : 'https://shielded-headland-62294.herokuapp.com';
 
 // url images:
-export const pathImage = isDebug ? '/uploads' : 'https://s3.eu-west-3.amazonaws.com/studento';
+export const pathImage = isDebug ? '/uploads' : URL_IMAGES;
 
 // Replace with 'UA-########-#' or similar to enable tracking
 export const trackingID = null;
