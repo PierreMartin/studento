091fcb3fb1447cca32c66d2a4b4e89035bc7e4db
Pierre Martin <pierre.martin.prestataire@sfr.com>
2019-01-22 15:08:20 +0100

    Settings account - implem popup for deleting account

--- app/pages/SettingsAccount.jsx
+++ app/pages/SettingsAccount.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { updateUserAction, emptyErrorsUserUpdateAction } from '../actions/userMe';
 import LayoutPage from '../components/layouts/LayoutPage/LayoutPage';
-import { Segment, Form, Message, Button } from 'semantic-ui-react';
+import { Segment, Form, Message, Button, Modal, Header, Icon } from 'semantic-ui-react';
 // import classNames from 'classnames/bind';
 // import styles from '../css/main.scss';
 
@@ -12,11 +12,16 @@ import { Segment, Form, Message, Button } from 'semantic-ui-react';
 class SettingsAccount extends Component {
 	constructor(props) {
 		super(props);
-		this.handleOnSubmit = this.handleOnSubmit.bind(this);
 		this.handleInputChange = this.handleInputChange.bind(this);
+		this.handleOnSubmit = this.handleOnSubmit.bind(this);
+		this.handleSubmitDeleteAccount = this.handleSubmitDeleteAccount.bind(this);
+
+		// modal:
+		this.handleOpenModalDeleteAccount = this.handleOpenModalDeleteAccount.bind(this);
 
 		this.state = {
-			fieldsTypingUpdateUser: {}
+			fieldsTypingUpdateUser: {},
+			isModalForDeleteAccountOpened: false
 		};
 	}
 
@@ -38,6 +43,7 @@ class SettingsAccount extends Component {
 		const fields = {};
 
 		fields.password = ((typeof fieldsTypingUpdateUser.password !== 'undefined') ? fieldsTypingUpdateUser.password : '') || null;
+		fields.passwordDelete = ((typeof fieldsTypingUpdateUser.passwordDelete !== 'undefined') ? fieldsTypingUpdateUser.passwordDelete : '') || null;
 		fields.email = ((typeof fieldsTypingUpdateUser.email !== 'undefined') ? fieldsTypingUpdateUser.email : userMe.email) || null;
 
 		return fields;
@@ -56,6 +62,15 @@ class SettingsAccount extends Component {
 		this.setState({ fieldsTypingUpdateUser: { [field.name]: field.value } });
 	}
 
+	handleOpenModalDeleteAccount() {
+		this.setState({ isModalForDeleteAccountOpened: !this.state.isModalForDeleteAccountOpened });
+	}
+
+	handleSubmitDeleteAccount() {
+		console.log('DELETE');
+		// this.props.deleteUserAccountAction(userMeId).then(() => { REDIRECTION } );
+	}
+
 	/**
 	 * Display a error if a field is wrong
 	 * @param {object} missingRequiredField - true if the field is missing
@@ -66,6 +81,7 @@ class SettingsAccount extends Component {
 		const errorsField = {};
 
 		if (missingRequiredField.password) errorsField.password = 'The password is required ';
+		if (missingRequiredField.passwordDelete) errorsField.passwordDelete = 'The password is required ';
 		if (missingRequiredField.email) errorsField.email = 'The mail is required ';
 		if (messageErrorState && messageErrorState.length > 0) errorsField.errorBackend = messageErrorState;
 
@@ -74,10 +90,11 @@ class SettingsAccount extends Component {
 
 	render() {
 		const { updateMissingRequiredField, updateMessageError } = this.props;
-		const { fieldsTypingUpdateUser } = this.state;
+		const { fieldsTypingUpdateUser, isModalForDeleteAccountOpened } = this.state;
 		const fields = this.getFieldsVal();
 		const messagesErrors = this.dispayFieldsErrors(updateMissingRequiredField, updateMessageError);
 		const disabledPasswordButton = typeof fieldsTypingUpdateUser.password === 'undefined' || fieldsTypingUpdateUser.password === '';
+		const disabledPasswordDelButton = typeof fieldsTypingUpdateUser.passwordDelete === 'undefined' || fieldsTypingUpdateUser.passwordDelete === '';
 		const disabledEmailButton = typeof fieldsTypingUpdateUser.email === 'undefined' || fieldsTypingUpdateUser.email === '';
 
 		return (
@@ -108,8 +125,36 @@ class SettingsAccount extends Component {
 
 				<div>
 					<h3>Delete account</h3>
-					<Button color="red" icon="delete" content="Delete your account" />
+					<Button color="red" icon="delete" content="Delete your account" onClick={this.handleOpenModalDeleteAccount} />
 				</div>
+
+				<Modal open={isModalForDeleteAccountOpened} onClose={this.handleOpenModalDeleteAccount}>
+					<Modal.Header>Delete account</Modal.Header>
+					<Modal.Content image>
+						<Modal.Description>
+							<Header>Are you sure to delete your account?</Header>
+
+							<Message icon color="red" size="mini">
+								<Icon name="attention" size="small" />
+								<Message.Content>This action will delete all your notes.</Message.Content>
+							</Message>
+
+							<p>Please type in your password to confirm.</p>
+
+							<Form error={updateMissingRequiredField.passwordDelete} size="small" onSubmit={this.handleSubmitDeleteAccount}>
+								<Form.Group>
+									<Form.Input width={4} required placeholder="Your Password" name="passwordDelete" value={fields.passwordDelete || ''} type="password" error={updateMissingRequiredField.passwordDelete} onChange={this.handleInputChange} />
+									<Form.Button width={8} disabled={disabledPasswordDelButton} color="red" content="I confirm delete my account" />
+								</Form.Group>
+								<Message error content={messagesErrors.passwordDelete} />
+							</Form>
+
+						</Modal.Description>
+					</Modal.Content>
+					<Modal.Actions>
+						<Button color="black" onClick={this.handleOpenModalDeleteAccount}>Cancel</Button>
+					</Modal.Actions>
+				</Modal>
 			</LayoutPage>
 		);
 	}
