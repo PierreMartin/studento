227dd34aefa394519bc448a6006ea84951e99294
Pierre Martin <pierremartin.pro@gmail.com>
2019-02-01 21:22:39 +0100

    change endpoint

--- config/app.js
+++ config/app.js
@@ -4,7 +4,7 @@ export const isProduction = ENV === 'production';
 export const isDebug = ENV === 'development';
 export const isClient = typeof window !== 'undefined';
 
-export const apiEndpoint = isDebug ? 'http://localhost:3000' : 'https://studento.herokuapp.com';
+export const apiEndpoint = isDebug ? 'http://localhost:3000' : 'https://hubnote.app';
 
 // url images:
 export const pathImage = isDebug ? '/uploads' : URL_IMAGES;
