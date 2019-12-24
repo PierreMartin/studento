332b7631f4392715f53b6e1adaed480f292dad7c
Pierre Martin <pierremartin.pro@gmail.com>
2019-12-24 18:22:36 +0100

    authFacebook - refacto

--- server/init/passport/facebook.js
+++ server/init/passport/facebook.js
@@ -6,72 +6,71 @@ export default (passport) => {
 		// 'profile' contain user profile information provided by Facebook
 		console.log('FB profile ', profile);
 
-		User.findOne({ 'facebook.id': profile.id }, (findByFacebookErr, findByFacebookUser) => {
-			console.log('1 ===> findByFacebookErr ', findByFacebookErr);
-			console.log('1 ===> findByFacebookUser ', findByFacebookUser);
-			if (findByFacebookUser) return done(null, findByFacebookUser);
+		const query = {
+			$or: [
+				{ 'facebook.id': profile.id },
+				{ email: { $in: profile.emails } }
+			]
+		};
 
-			return User.findOne({ email: { $in: profile.emails } }, (findByEmailErr, findByEmailUser) => {
-				console.log('1 ===> findByEmailErr ', findByEmailErr);
-				console.log('1 ===> findByEmailUser ', findByEmailUser);
-				if (findByEmailErr) { return done(findByEmailErr); }
+		User.findOne(query, (findErr, findUser) => {
+			console.log('1 ===> findErr ', findErr);
+			console.log('1 ===> findUser ', findUser);
 
-				// No user was found: create a new user with values from Facebook
-				if (!findByEmailUser) {
-					const userObj = {
-						provider: 'facebook',
-						facebook: profile._json,
-						username: '',
-						avatarsSrc: [],
-						avatarMainSrc: {
-							avatarId: -1,
-							avatar150: '',
-							avatar80: '',
-							avatar28: ''
-						}
-					};
+			if (findErr) { return done(findErr); }
+			if (findUser) { return done(null, findUser); }
 
-					if (profile.emails && profile.emails[0] && profile.emails[0].value) {
-						// userObj.email = `${profile.emails[0].value}`;
-					}
-					if (profile._json && profile.displayName) {
-						userObj.username = profile.displayName;
-					}
-					if (profile.name && profile.name.givenName) {
-						userObj.firstName = profile.name.givenName;
-					}
-					if (profile.name && profile.name.familyName) {
-						userObj.lastName = profile.name.familyName;
-					}
-					if (profile && profile.photos && profile.photos[0] && profile.photos[0].value) {
-						userObj.avatarsSrc.push({
-							avatarId: 0,
-							avatar150: profile.photos[0].value,
-							avatar80: profile.photos[0].value,
-							avatar28: profile.photos[0].value,
-							provider: 'facebook'
-						});
-
-						userObj.avatarMainSrc.avatarId = 0;
-						userObj.avatarMainSrc.avatar150 = profile.photos[0].value;
-						userObj.avatarMainSrc.avatar80 = profile.photos[0].value;
-						userObj.avatarMainSrc.avatar28 = profile.photos[0].value;
-						userObj.avatarMainSrc.provider = 'facebook';
-					}
-
-					userObj.username += `-${Math.ceil(Math.random() * 100000)}`;
-					console.log('1 === userObj ', userObj);
-
-					const user = new User(userObj);
-					user.save((err) => {
-						console.log('2 record db err', err);
-						console.log('2 record db user', user);
-						if (err) { console.log(err); }
-						return done(null, user);
-					});
-				} else {
-					return done(null, findByEmailUser);
+			// No user was found: create a new user with values from Facebook
+			const userObj = {
+				provider: 'facebook',
+				facebook: profile._json,
+				username: '',
+				avatarsSrc: [],
+				avatarMainSrc: {
+					avatarId: -1,
+					avatar150: '',
+					avatar80: '',
+					avatar28: ''
 				}
+			};
+
+			if (profile.emails && profile.emails[0] && profile.emails[0].value) {
+				// userObj.email = `${profile.emails[0].value}`;
+			}
+			if (profile._json && profile.displayName) {
+				userObj.username = profile.displayName;
+			}
+			if (profile.name && profile.name.givenName) {
+				userObj.firstName = profile.name.givenName;
+			}
+			if (profile.name && profile.name.familyName) {
+				userObj.lastName = profile.name.familyName;
+			}
+			if (profile && profile.photos && profile.photos[0] && profile.photos[0].value) {
+				userObj.avatarsSrc.push({
+					avatarId: 0,
+					avatar150: profile.photos[0].value,
+					avatar80: profile.photos[0].value,
+					avatar28: profile.photos[0].value,
+					provider: 'facebook'
+				});
+
+				userObj.avatarMainSrc.avatarId = 0;
+				userObj.avatarMainSrc.avatar150 = profile.photos[0].value;
+				userObj.avatarMainSrc.avatar80 = profile.photos[0].value;
+				userObj.avatarMainSrc.avatar28 = profile.photos[0].value;
+				userObj.avatarMainSrc.provider = 'facebook';
+			}
+
+			userObj.username += `-${Math.ceil(Math.random() * 100000)}`;
+			console.log('1 === userObj ', userObj);
+
+			const user = new User(userObj);
+			user.save((err) => {
+				console.log('2 record db err', err);
+				console.log('2 record db user', user);
+				if (err) { console.log(err); }
+				return done(null, user);
 			});
 		});
 	};
